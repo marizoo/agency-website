402d8a4ed3ea74635bcd9fbf85fdffe6fb408121
marizoo <marizoo@meow.woof>
2021-12-10 21:56:31 +0700

    components: Announcement.jsx > Done

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -3,23 +3,46 @@ import styled from 'styled-components'
 import { mainColor, mobile } from '../globalStyle'
 
 const Container = styled.div`
-height: 30px;
+height: 40px;
 width: 100vw;
 background-color: ${mainColor};
+display: flex;
+justify-content: space-between;
+align-items: center;
+`
+
+const TextContainer = styled.div`
+width: 100%;
 display: flex;
 justify-content: center;
 align-items: center;
 `
 
-const Text = styled.div`
+const Text = styled.p`
+color: white;
+`
+
+const Close = styled.div`
+justify-self: end;
+height: 100%;
+width: 40px;
+display: flex;
+justify-content: center;
+align-items: center;
+background-color: #4e4e4e;
 color: white;
 `
 
 const Announcement = () => {
     return (
+    
         <Container>
-            <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim in fugiat praesentium quasi accusantium at!z</Text>
+            <TextContainer>
+                <Text>Lorem ipsum dolor. Enim in fugiat praesentium quasi accusantium at!z</Text>
+            </TextContainer>
+            <Close>x</Close>
         </Container>
+    
     )
 }
 
