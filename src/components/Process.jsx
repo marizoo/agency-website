d24c2542d5e9548ddf785695f3b8b37bd1930e0e
marizoo <marizoo@meow.woof>
2021-12-11 14:33:47 +0700

    components: Process.jsx > Done

--- src/components/Process.jsx
+++ src/components/Process.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import styled from 'styled-components'
+import {mainColor, secondaryColor} from '../globalStyle'
 import WbIncandescentOutlinedIcon from '@material-ui/icons/WbIncandescentOutlined';
 import EventOutlinedIcon from '@material-ui/icons/EventOutlined';
 import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import { pink } from '@material-ui/core/colors';
 
 const Container = styled.div`
 height: 700px;
@@ -13,46 +15,73 @@ flex-direction: column;
 
 const Top = styled.div`
 height: 20%;
-background-color: pink;
+display: flex;
+flex-direction: center;
+align-items: center;
 `
 
 const TitleContainer = styled.div`
-width: 60%;
+width: 100%;
+height: 100%;
 display: flex;
 justify-content: center;
 align-items: flex-end; 
+
 `
 
 const Title = styled.h2`
 letter-spacing: 1px;
+max-width: 40%;
+text-align:center;
 `
 
 const Bottom = styled.div`
 height: 80%;
-background-color: lightblue;
 display: flex;
-justify-content: space-around;
+justify-content: center;
 align-items: center;
 `
 
 const Card = styled.div`
 display: flex;
 flex-direction: column;
-justify-content: center;
+justify-content: space-around;
 align-items: center;
+margin: 0 15px;
+width: 20%;
+height: 90%;
+padding: 20px;
 `
 
-const CardIcon = styled.div``
+const CardIcon = styled.div`
+transform: ${props => (props.isTurn ? "rotate(180deg)" : "")};
+
+`
 
 const iconStyle = {
     fontSize: "100px",
+    color: "#8a7500",
+    fontWeight: "200"
+   
 }
 
-const CardTitle = styled.div``
+const CardTitle = styled.h2`
+font-size: 40px;
+font-family: 'Satisfy', cursive;
 
-const Desc = styled.div``
+`
 
-const Button = styled.div``
+const Desc = styled.p`
+text-align: center;
+font-size: 18px;
+`
+
+const Button = styled.div`
+background-color: ${mainColor};
+padding: 10px;
+color: white;
+font-weight: 500;
+`
 
 const Process = () => {
     return (
@@ -68,12 +97,12 @@ const Process = () => {
             </Top>
             <Bottom>
                 <Card>
-                    <CardIcon>
+                    <CardIcon isTurn={true}>
                         <WbIncandescentOutlinedIcon style={iconStyle}/>
                     </CardIcon>
                     <CardTitle>Build</CardTitle>
                     <Desc>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis!
+                        Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet.consectetur adipisicing elit. Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis!
                     </Desc>
                     <Button>Show me the money!</Button>
                 </Card>
@@ -83,7 +112,7 @@ const Process = () => {
                     </CardIcon>
                     <CardTitle>Launch</CardTitle>
                     <Desc>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis! Lorem, ipsum dolor.
                     </Desc>
                     <Button>Show me the money!</Button>
                 </Card>
@@ -93,7 +122,7 @@ const Process = () => {
                     </CardIcon>
                     <CardTitle>Grow</CardTitle>
                     <Desc>
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis!
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet.Quis suscipit dolor fuga ut inventore minus modi error ab neque reiciendis!
                     </Desc>
                     <Button>Show me the money!</Button>
                 </Card>
